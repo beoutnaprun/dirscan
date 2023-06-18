bf09b26c9b692d5eef1428687ab35319481c1fc9
beoutnaprun <133112969+beoutnaprun@users.noreply.github.com>
2023-06-18 17:40:22 +0800

    Update dirscan.py

--- dirscan/dirscan.py
+++ dirscan/dirscan.py
@@ -1,25 +1,29 @@
 import concurrent.futures, json
+import sys
+
 import requests, urllib3, argparse, random
 
-urllib3.disable_warnings()
-
 name = '''
-
-    |   |        |   |                     |   |  /   /
-    |   |        |   |                     |   | /   /
-    |   |________|   |  _______       _____|   |/   /   _______   ___  _____
-    |    ________    |/  ____  \    /  ____|   |   /   /  ___   \|   |/ __  \ 
-    |   |        |   |  /    \  \  /  /    |   |   \  /  /___\   |     /  \_/ 
-    |   |        |   | |      |  \/  /     |   |\   \/  _________|    /  
-    |   |        |   |  \____/ /\ \  \_____|   | \   \  \________|   |          
-    |___|        |___|\_______/  \_\_______|___|  \___\__________|___|          
-
-    Hello, welcome to use the url DirScan tool of Chinese hacker X 
+            
+    |   |       |   |                             __________
+    |   |       |   |                           /   -----   \          
+    |   |_______|   |  _____           ______  /   /     \___\     _____     _______
+    |    _______    |/  ___  \  \  /  /  ___  \    |             /      \  /   ___  \ 
+    |   |       |   |  |___\  \  \/  /  /   \  \   |     ______ /   ___  \/   /   \  \ 
+    |   |       |   |  _____   |    |  |     |  \   \   |___   |   |___|  |  |     |  |
+    |   |       |   |  \___/  /  /\  \  \___/ /\ \   \_____/  / \        /|  |     |  |
+    |___|       |___|\_______/  /  \  \______/  \_\__________/   \______/ |  |     |  |
+    
+    Hello, welcome to use the DirScan tool of HexaGon Laboratory
+    The author of this article China Hacker X
+    
     Please enter the - h parameter for help
+    
 '''
+
 parser = argparse.ArgumentParser(description='This is the help!')
 parser.add_argument('-u', '--url', help='\t要检测的url', default='')
-parser.add_argument('-t', '--thread', help='\t指定线程 默认30', default='30')
+parser.add_argument('-t', '--thread', help='\t指定线程 默认2', default='2')
 parser.add_argument('-p', '--proxy', nargs='?', const='127.0.0.1:5000', default=None,
                     help='指定是否使用代理 无参数自动5000端口代理 指定格式:127.0.0.1:5000')
 parser.add_argument('-d', '--dings', nargs='?', const='', default="None",
@@ -37,7 +41,7 @@ def req_list(urls):
         if i.startswith("/"):
             url = urls + i
         else:
-            url = urls + "/" + i
+            url = urls +"/"+ i
         if url not in list:
             list.append(url)
     return list
@@ -69,7 +73,7 @@ def request_url(url, proxy):
             "user-agent": f"{getuseragemt()}"
         }
         if proxy == "d":
-            req = requests.get(url, verify=False, allow_redirects=False, headers=header, proxies=proxys, timeout=5)
+            req = requests.get(url, verify=False, allow_redirects=False, headers=header, proxies=proxys, timeout=10)
             if req.status_code == 404:
                 # print(f"\033[1;36;40m{req.status_code}        {url}\033[0m")
                 pass
@@ -90,7 +94,7 @@ def request_url(url, proxy):
             else:
                 print(f"\033[1;36;40m{req.status_code}        {url}\033[0m")
         else:
-            req = requests.get(url, verify=False, allow_redirects=False, headers=header, timeout=5)
+            req = requests.get(url, verify=False, allow_redirects=False, headers=header, timeout=10)
             if req.status_code == 404:
                 # print(f"\033[1;36;40m{req.status_code}        {url}\033[0m")
                 pass
@@ -111,8 +115,10 @@ def request_url(url, proxy):
             else:
                 print(f"\033[1;36;40m{req.status_code}        {url}\033[0m")
     except requests.exceptions.RequestException as e:
-        print(f"Error : {url}: {str(e)}")
-        # pass
+        # print(f"Error:{url}--连接被终止")
+        pass
+
+
 
 
 # 创建 爆破线程
@@ -122,15 +128,15 @@ def threq(urls, num_threads, proxy):
         # 提交每个URL的请求任务给线程池
         futures = [executor.submit(request_url, url, proxy) for url in urls]
         # 等待所有请求任务执行完毕
-        concurrent.futures.wait(futures)
+    concurrent.futures.wait(futures)
     print("All requests have finished execution.")
-
 
 # 第一次请求给出的地址
 def onereq(url):
     header = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36"
     }
+
     reqs = requests.get(url, headers=header, verify=False, allow_redirects=False, timeout=3)
     print(f"\033[1;32;40m{reqs.status_code}        {url}\033[0m")
 
@@ -144,6 +150,11 @@ def run():
         a = input("给出的地址 无效访问~~~是否继续爆破(Y/N)")
     if a == "y" or a == "Y":
         if args.url != "":
+            last_url = args.url[-1]
+            if last_url == "/":
+                urls = args.url[:-1]
+            else:
+                urls = args.url
             if args.proxy:
                 proxy = {
                     "http": args.proxy
@@ -151,12 +162,12 @@ def run():
                 print(
                     f"\033[1;36;40m爆破地址: \033[0m \033[1;34;40m{args.url}\033[0m      \033[1;36;40m爆破线程: \033[0m \033[1;34;40m{args.thread}\033[0m  \033[1;36;40m爆破代理: \033[0m \033[1;34;40m{args.proxy}\033[0m ")
                 print("=" * 30 + "正在爆破目录~~请稍等~~" + "=" * 30)
-                threq(req_list(args.url), int(args.thread), proxy)
+                threq(req_list(urls), int(args.thread), proxy)
             else:
                 print(
                     f"\033[1;36;40m爆破地址: \033[0m \033[1;34;40m{args.url}\033[0m      \033[1;36;40m爆破线程: \033[0m \033[1;34;40m{args.thread}\033[0m  \033[1;36;40m爆破代理: \033[0m \033[1;34;40m无\033[0m ")
                 print("=" * 30 + "正在爆破目录~~请稍等~~" + "=" * 30)
-                threq(req_list(args.url), int(args.thread), {})
+                threq(req_list(urls), int(args.thread), {})
         else:
             print("Error : 请使用-h查看帮助信息")
 
@@ -169,11 +180,16 @@ def runproxy():
         a = input("给出的地址 无效访问~~~是否继续爆破(Y/N)")
     if a == "y" or a == "Y":
         if args.url != "":
+            last_url = args.url[-1]
+            if last_url == "/":
+                urls = args.url[:-1]
+            else:
+                urls = args.url
             # s = openfile()[random.randint(0, len(a) - 1)]
             print(
-                f"\033[1;36;40m爆破地址: \033[0m \033[1;34;40m{args.url}\033[0m      \033[1;36;40m爆破线程: \033[0m \033[1;34;40m{args.thread}\033[0m  \033[1;36;40m爆破代理: \033[0m \033[1;34;40m无\033[0m ")
+                f"\033[1;36;40m爆破地址: \033[0m \033[1;34;40m{args.url}\033[0m      \033[1;36;40m爆破线程: \033[0m \033[1;34;40m{args.thread}\033[0m  \033[1;36;40m爆破代理: \033[0m \033[1;34;40m代理池\033[0m ")
             print("=" * 30 + "正在爆破目录~~请稍等~~" + "=" * 30)
-            threq(req_list(args.url), int(args.thread), "d")
+            threq(req_list(urls), int(args.thread), "d")
         else:
             print("Error : 请使用-h查看帮助信息")
 
@@ -184,3 +200,5 @@ if __name__ == '__main__':
         run()
     else:
         runproxy()
+
+    sys.exit()
